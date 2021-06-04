d88cf5d81869b6580d4f81517f22ec80b152cc8a
Filippo Dossena <filippo.dossena@mozart.it>
2021-06-05 00:26:03 +0200

    Add minimal test

---
+++ test/__helpers/SimpleForm.tsx
@@ -0,0 +1,57 @@
+import { observer } from "mobx-react-lite"
+import * as React from "react"
+import { useForm } from "../../src"
+
+export const SimpleForm = observer(() => {
+  const form = useForm(
+    {
+      username: "",
+      password: "",
+    },
+    {
+      onSubmit: values => {
+        console.log("submitted", values)
+      },
+      onValidate: async ({ username, password }) => {
+        let errors = {} as Record<string, string>
+        if (username === "") {
+          errors.username = "Username cannot be blank."
+        }
+        if (password === "") {
+          errors.password = "Password cannot be blank."
+        }
+        return errors
+      },
+    }
+  )
+
+  const { Form, Field } = form
+
+  return (
+    <div>
+      <Form debug>
+        <Field name="username">
+          {field => (
+            <div>
+              <input type="text" autoComplete="username" {...field.input} />
+              {field.meta.touched && field.meta.error}
+            </div>
+          )}
+        </Field>
+        <Field name="password">
+          {field => (
+            <div>
+              <input
+                type="password"
+                autoComplete="current-password"
+                {...field.input}
+              />
+              {field.meta.touched && field.meta.error}
+            </div>
+          )}
+        </Field>
+        <button type="submit">Login</button>
+      </Form>
+    </div>
+  )
+})
