a7b2313bc89f684d3bce45c3cc83831f65693b82
Filippo Dossena <filippo.dossena@mozart.it>
2021-06-04 23:13:49 +0200

    Add minimal example

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,60 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import "react-app-polyfill/ie11"
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import { useForm } from "../."
 
 const App = () => {
+  const form = useForm(
+    {
+      username: "",
+      password: "",
+    },
+    {
+      onSubmit: values => {
+        console.log("submitted", values)
+      },
+      onValidate: async ({ username, password }) => {
+        let errors = {} as Record<string, string>
+        if (username === "") {
+          errors.username = "Username cannot be blank."
+        }
+        if (password === "") {
+          errors.password = "Password cannot be blank."
+        }
+        return errors
+      },
+    }
+  )
+
+  const { Form, Field } = form
+
   return (
     <div>
-      <Thing />
+      <Form>
+        <Field name="username">
+          {field => (
+            <div>
+              <input type="text" autoComplete="username" {...field.input} />
+              {field.meta.touched && field.meta.error}
+            </div>
+          )}
+        </Field>
+        <Field name="password">
+          {field => (
+            <div>
+              <input
+                type="password"
+                autoComplete="current-password"
+                {...field.input}
+              />
+              {field.meta.touched && field.meta.error}
+            </div>
+          )}
+        </Field>
+        <button type="submit">Login</button>
+      </Form>
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"))
