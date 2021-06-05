2553fcad2c2db80c6751ba04173eec0633e7fa38
Filippo Dossena <filippo.dossena@mozart.it>
2021-06-05 12:38:39 +0200

    Move utils functions into its own file

--- src/useForm.tsx
+++ src/useForm.tsx
@@ -6,70 +6,13 @@ import { Observer, observer, useLocalObservable } from "mobx-react-lite"
 import React, { ReactElement, useMemo, useRef } from "react"
 import isEqual from "react-fast-compare"
 import { DebugForm } from "./DebugForm"
-
-const isString = (obj: any): obj is string =>
-  Object.prototype.toString.call(obj) === "[object String]"
-
-const isFunction = (func: any): func is Function => func instanceof Function
-
-const isObject = (obj: any): obj is Object =>
-  obj !== null && typeof obj === "object"
-
-function setNestedObjectValues<T>(
-  object: any,
-  value: any,
-  visited: any = new WeakMap(),
-  response: any = {}
-): T {
-  for (let k of Object.keys(object)) {
-    const val = object[k]
-    if (isObject(val)) {
-      if (!visited.get(val)) {
-        visited.set(val, true)
-        // In order to keep array values consistent for both dot path  and
-        // bracket syntax, we need to check if this is an array so that
-        // this will output  { friends: [true] } and not { friends: { "0": true } }
-        response[k] = Array.isArray(val) ? [] : {}
-        setNestedObjectValues(val, value, visited, response[k])
-      }
-    } else {
-      response[k] = value
-    }
-  }
-
-  return response
-}
-
-function getValueForCheckbox(
-  currentValue: string | any[],
-  checked: boolean,
-  valueProp: any
-) {
-  // eslint-disable-next-line eqeqeq
-  if (valueProp == "true" || valueProp == "false") {
-    return !!checked
-  }
-
-  if (checked && valueProp) {
-    return Array.isArray(currentValue)
-      ? currentValue.concat(valueProp)
-      : [valueProp]
-  }
-  if (!Array.isArray(currentValue)) {
-    return !currentValue
-  }
-  const index = currentValue.indexOf(valueProp)
-  if (index < 0) {
-    return currentValue
-  }
-  return currentValue.slice(0, index).concat(currentValue.slice(index + 1))
-}
-
-function getSelectedValues(options: any[]) {
-  return Array.from(options)
-    .filter(el => el.selected)
-    .map(el => el.value)
-}
+import {
+  getSelectedValues,
+  getValueForCheckbox,
+  isFunction,
+  isString,
+  setNestedObjectValues,
+} from "./utils"
 
 export type FormModel = {
   [field: string]: any
