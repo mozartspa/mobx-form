4248114db9f05a26c12e7af3c9308301c7931a44
Filippo Dossena <filippo.dossena@mozart.it>
2021-06-09 18:58:39 +0200

    Add ArrayForm to example

--- example/App.tsx
+++ example/App.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Container, Jumbotron, Nav, Navbar } from "react-bootstrap"
 import { LinkContainer } from "react-router-bootstrap"
 import { HashRouter, Route, Switch } from "react-router-dom"
+import { ArrayForm } from "./components/ArrayForm"
 import { LoginForm } from "./components/LoginForm"
 import { PerformantForm } from "./components/PerformantForm"
 
@@ -14,16 +15,22 @@ export const App = () => {
         </Jumbotron>
         <Navbar bg="dark" variant="dark">
           <Nav className="mr-auto">
-            <LinkContainer to="/">
+            <LinkContainer to="/" exact>
               <Nav.Link>Login form</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/performant-form">
               <Nav.Link>Performant form</Nav.Link>
             </LinkContainer>
+            <LinkContainer to="/array-form">
+              <Nav.Link>Array form</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar>
         <Container className="border border-2 p-4 ">
           <Switch>
+            <Route path="/array-form">
+              <ArrayForm />
+            </Route>
             <Route path="/performant-form">
               <PerformantForm />
             </Route>
